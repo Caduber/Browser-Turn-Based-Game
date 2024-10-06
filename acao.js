ce313737b49b70e37fab49d21128c9a269d04df6
Carlos Eduardo Drago <carlosemdl@gmail.com>
2024-10-06 02:08:44 -0300

    Combate Goblin

--- acao.js
+++ acao.js
@@ -3,21 +3,6 @@ goblinHp = document.getElementById("hp-goblin-atual")
 texto = document.getElementById("texto-luta")
 spriteHeroi = document.getElementById("sprite-heroi")
 
-function tomarDano(danoTomado) {
-    heroi.hp = heroi.hp - danoTomado
-// Corrigindo para valores negativos
-    if(heroi.hp <= 0){
-        heroi.hp = 0
-        spriteHeroi.style.animationName = "a"
-    }
-//    
-    barraHp.style.width = heroi.hp + '%'
-    goblin.multiplicador = 1
-    console.log(heroi.hp)
-}
-
-
-
 function darDano() {
     goblin.hp = goblin.hp - (heroi.dmg * heroi.multiplicador)
 // Corrigindo para valores negativos
--- acaoGoblin.js
+++ acaoGoblin.js
@@ -1,1 +1,20 @@
-addEventListener("")
+barraHp = document.getElementById("hp-heroi-atual")
+goblinHp = document.getElementById("hp-goblin-atual")
+texto = document.getElementById("texto-luta")
+
+acionamento = document.getElementById("acoes")
+acionamento.addEventListener("click", tomarDano)
+
+
+function tomarDano() {
+    heroi.hp = heroi.hp - goblin.dmg
+// Corrigindo para valores negativos
+    if(heroi.hp <= 0){
+        heroi.hp = 0
+        spriteHeroi.style.animationName = "a"
+    }
+//    
+    barraHp.style.width = heroi.hp + '%'
+    goblin.multiplicador = 1
+    console.log(heroi.hp)
+}
