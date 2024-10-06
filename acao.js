39260eeafb6c1daa6f0593ecdd3662c36b082263
Carlos Eduardo Drago <carlosemdl@gmail.com>
2024-10-06 01:26:37 -0300

    first commit

---
+++ acao.js
@@ -0,0 +1,54 @@
+barraHp = document.getElementById("hp-heroi-atual")
+goblinHp = document.getElementById("hp-goblin-atual")
+texto = document.getElementById("texto-luta")
+
+function tomarDano(danoTomado) {
+    heroi.hp = heroi.hp - danoTomado
+// Corrigindo para valores negativos
+    if(heroi.hp <= 0){
+        heroi.hp = 0
+    }
+//    
+    barraHp.style.width = heroi.hp + '%'
+    goblin.multiplicador = 1
+    console.log(heroi.hp)
+}
+
+
+
+function darDano() {
+    goblin.hp = goblin.hp - (heroi.dmg * heroi.multiplicador)
+// Corrigindo para valores negativos
+    if(goblin.hp <= 0){
+        goblin.hp = 0
+    }
+//
+    goblinHp.style.width = (goblin.hp / 60) * 100 + "%"
+
+    texto.innerHTML = "O oponente sofreu " + (heroi.dmg * heroi.multiplicador) + " de dano!"
+    heroi.multiplicador = 1
+    console.log(goblin.hp)
+}
+
+function prepararAtaque(){
+    heroi.multiplicador = heroi.multiplicador * 1.5
+    texto.innerHTML =  "O heroi preparou o ataque..."
+
+}
+
+function assustarInimigo(){
+    goblin.multiplicador = goblin.multiplicador * 0.75
+    texto.innerHTML =  "O inimigo está fragilizado"
+}
+
+function enfaixarFeridas(){
+    heroi.hp = heroi.hp + 10
+// Corrigindo para valores maiores q o hp max
+    if(heroi.hp > 100){
+        heroi.hp = 100
+    }
+//
+    barraHp.style.width = heroi.hp + '%'
+    texto.innerHTML =  "O heroi está se sentindo melhor"
+}
+
